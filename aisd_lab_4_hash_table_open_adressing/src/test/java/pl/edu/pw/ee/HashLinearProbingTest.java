e1c5266eeece912b6c6c6e9a49abe2098f08f038
Daniel Ślusarczyk <daniel.slusarczyk.stud@pw.edu.pl>
2021-11-13 18:56:47 +0100

    Added tests

--- aisd_lab_4_hash_table_open_adressing/src/test/java/pl/edu/pw/ee/HashLinearProbingTest.java
+++ aisd_lab_4_hash_table_open_adressing/src/test/java/pl/edu/pw/ee/HashLinearProbingTest.java
@@ -69,14 +69,43 @@ public class HashLinearProbingTest {
     }
 
     @Test
+    public void should_CorrectlyDoubleSize_IfNeeded() {
+        // given
+        int size = 100;
+        doubleHash = new HashLinearProbing<>(size);
+        int testLength = (int) (doubleHash.getCorrectLoadFactor() * size) + 1;
+        long SEED = 1410;
+        Random random = new Random(SEED);
+        List<Double> doubleList = new ArrayList<>();
+
+        // when
+        int sizeBefore = doubleHash.getSize();
+        for (int i = 0; i < testLength; i++) {
+            double randomDouble = random.nextDouble();
+            while (doubleList.contains(randomDouble)) {
+                randomDouble = random.nextDouble();
+            }
+            doubleHash.put(random.nextDouble());
+            doubleList.add(randomDouble);
+        }
+        int sizeAfter = doubleHash.getSize();
+
+        // then
+        int expectedBefore = size;
+        int expectedAfter = size * 2;
+        assertEquals(expectedBefore, sizeBefore);
+        assertEquals(expectedAfter, sizeAfter);
+    }
+
+    @Test
     public void should_CorrectlyAddNewStringElems_WhenNotExistInHashTable() {
         // given
         String newEleme = "nothing special";
 
         // when
-        int nOfElemsBeforePut = stringHash.getNumOfElems();
+        int nOfElemsBeforePut = stringHash.getNElem();
         stringHash.put(newEleme);
-        int nOfElemsAfterPut = stringHash.getNumOfElems();
+        int nOfElemsAfterPut = stringHash.getNElem();
 
         // then
         int expectedBeforePut = 0;
@@ -91,9 +120,9 @@ public class HashLinearProbingTest {
         double newEleme = 1.0;
 
         // when
-        int nOfElemsBeforePut = doubleHash.getNumOfElems();
+        int nOfElemsBeforePut = doubleHash.getNElem();
         doubleHash.put(newEleme);
-        int nOfElemsAfterPut = doubleHash.getNumOfElems();
+        int nOfElemsAfterPut = doubleHash.getNElem();
 
         // then
         int expectedBeforePut = 0;
@@ -109,10 +138,10 @@ public class HashLinearProbingTest {
         String secondEleme = "nothing special too";
 
         // when
-        int nOfElemsBeforePut = stringHash.getNumOfElems();
+        int nOfElemsBeforePut = stringHash.getNElem();
         stringHash.put(firstEleme);
         stringHash.put(secondEleme);
-        int nOfElemsAfterPut = stringHash.getNumOfElems();
+        int nOfElemsAfterPut = stringHash.getNElem();
 
         // then
         int expectedBeforePut = 0;
@@ -128,10 +157,10 @@ public class HashLinearProbingTest {
         double secondEleme = 1.0;
 
         // when
-        int nOfElemsBeforePut = doubleHash.getNumOfElems();
+        int nOfElemsBeforePut = doubleHash.getNElem();
         doubleHash.put(firstEleme);
         doubleHash.put(secondEleme);
-        int nOfElemsAfterPut = doubleHash.getNumOfElems();
+        int nOfElemsAfterPut = doubleHash.getNElem();
 
         // then
         int expectedBeforePut = 0;
@@ -147,10 +176,10 @@ public class HashLinearProbingTest {
         String secondEleme = "nothing special";
 
         // when
-        int nOfElemsBeforePut = stringHash.getNumOfElems();
+        int nOfElemsBeforePut = stringHash.getNElem();
         stringHash.put(firstEleme);
         stringHash.put(secondEleme);
-        int nOfElemsAfterPut = stringHash.getNumOfElems();
+        int nOfElemsAfterPut = stringHash.getNElem();
 
         // then
         int expectedBeforePut = 0;
@@ -165,11 +194,11 @@ public class HashLinearProbingTest {
         int testLength = 100;
 
         // when
-        int nOfElemsBeforePut = stringHash.getNumOfElems();
+        int nOfElemsBeforePut = stringHash.getNElem();
         for (int i = 0; i < testLength; i++) {
             stringHash.put(RandomStringUtils.randomAlphanumeric(10));
         }
-        int nOfElemsAfterPut = stringHash.getNumOfElems();
+        int nOfElemsAfterPut = stringHash.getNElem();
 
         // then
         int expectedBeforePut = 0;
@@ -184,11 +213,11 @@ public class HashLinearProbingTest {
         int testLength = 100;
 
         // when
-        int nOfElemsBeforePut = doubleHash.getNumOfElems();
+        int nOfElemsBeforePut = doubleHash.getNElem();
         for (int i = 0; i < testLength; i++) {
             doubleHash.put(new Random().nextDouble());
         }
-        int nOfElemsAfterPut = doubleHash.getNumOfElems();
+        int nOfElemsAfterPut = doubleHash.getNElem();
 
         // then
         int expectedBeforePut = 0;
@@ -202,12 +231,13 @@ public class HashLinearProbingTest {
         // given
         int testLength = 100;
         String toPut = RandomStringUtils.randomAlphanumeric(10);
+
         // when
-        int nOfElemsBeforePut = stringHash.getNumOfElems();
+        int nOfElemsBeforePut = stringHash.getNElem();
         for (int i = 0; i < testLength; i++) {
             stringHash.put(toPut);
         }
-        int nOfElemsAfterPut = stringHash.getNumOfElems();
+        int nOfElemsAfterPut = stringHash.getNElem();
 
         // then
         int expectedBeforePut = 0;
@@ -221,12 +251,13 @@ public class HashLinearProbingTest {
         // given
         int testLength = 100;
         double toPut = 1.0;
+
         // when
-        int nOfElemsBeforePut = doubleHash.getNumOfElems();
+        int nOfElemsBeforePut = doubleHash.getNElem();
         for (int i = 0; i < testLength; i++) {
             doubleHash.put(toPut);
         }
-        int nOfElemsAfterPut = doubleHash.getNumOfElems();
+        int nOfElemsAfterPut = doubleHash.getNElem();
 
         // then
         int expectedBeforePut = 0;
@@ -236,46 +267,18 @@ public class HashLinearProbingTest {
     }
 
     @Test
-    public void should_CorrectlyDoubleSize_IfNeeded() {
-        // given
-        int size = 100;
-        doubleHash = new HashLinearProbing<>(size);
-        int testLength = (int) (doubleHash.getCorrectLoadFactor() * size) + 1;
-        long SEED = 1410;
-        Random random = new Random(SEED);
-        List<Double> doubleList = new ArrayList<>();
-        // when
-
-        int sizeBefore = doubleHash.getSize();
-        for (int i = 0; i < testLength; i++) {
-            double randomDouble = random.nextDouble();
-            while (doubleList.contains(randomDouble)) {
-                randomDouble = random.nextDouble();
-            }
-            doubleHash.put(random.nextDouble());
-            doubleList.add(randomDouble);
-        }
-        int sizeAfter = doubleHash.getSize();
-
-        // then
-        int expectedBefore = size;
-        int expectedAfter = size * 2;
-        doubleHash.print();
-        assertEquals(expectedBefore, sizeBefore);
-        assertEquals(expectedAfter, sizeAfter);
-    }
-
-    @Test
     public void should_CorrectlyGetDoubleElems_WhenExistInHashTable() {
         // given
         double toAdd = 1.687;
         double toGet = toAdd;
+
         // when
         doubleHash.put(toAdd);
+        double actualValue = doubleHash.get(toGet);
+
         // then
-        double expected = toGet;
-        double actual = doubleHash.get(toGet);
-        assertEquals(expected, actual, 0);
+        double expectedValue = toGet;
+        assertEquals(expectedValue, actualValue, 0);
     }
 
     @Test
@@ -283,12 +286,14 @@ public class HashLinearProbingTest {
         // given
         String toAdd = "toAdd";
         String toGet = toAdd;
+
         // when
         stringHash.put(toAdd);
+        String actualValue = stringHash.get(toGet);
+
         // then
-        String expected = toGet;
-        String actual = stringHash.get(toGet);
-        assertEquals(expected, actual);
+        String expectedValue = toGet;
+        assertEquals(expectedValue, actualValue);
     }
 
     @Test
@@ -298,20 +303,23 @@ public class HashLinearProbingTest {
         long SEED = 1410;
         Random random = new Random(SEED);
         List<Double> doubleList = new ArrayList<>();
+
         // when
         for (int i = 0; i < testLength; i++) {
             double randomDouble = random.nextDouble();
             while (doubleList.contains(randomDouble)) {
                 randomDouble = random.nextDouble();
             }
+
             doubleList.add(randomDouble);
             doubleHash.put(randomDouble);
         }
+        
         // then
         for (int i = 0; i < testLength; i++) {
-            double expected = doubleList.get(i);
-            double actual = doubleHash.get(doubleList.get(i));
-            assertEquals(expected, actual, 0);
+            double expectedValue = doubleList.get(i);
+            double actualValue = doubleHash.get(doubleList.get(i));
+            assertEquals(expectedValue, actualValue, 0);
         }
     }
 
@@ -320,20 +328,23 @@ public class HashLinearProbingTest {
         // given
         int testLength = 100;
         List<String> stringList = new ArrayList<>();
+
         // when
         for (int i = 0; i < testLength; i++) {
             String randomString = RandomStringUtils.randomAlphanumeric(10);
             while (stringList.contains(randomString)) {
                 randomString = RandomStringUtils.randomAlphanumeric(10);
             }
+
             stringList.add(randomString);
             stringHash.put(randomString);
         }
+
         // then
         for (int i = 0; i < testLength; i++) {
-            String expected = stringList.get(i);
-            String actual = stringHash.get(stringList.get(i));
-            assertEquals(expected, actual);
+            String expectedValue = stringList.get(i);
+            String actualValue = stringHash.get(stringList.get(i));
+            assertEquals(expectedValue, actualValue);
         }
     }
 
@@ -345,19 +356,23 @@ public class HashLinearProbingTest {
         Random random = new Random(SEED);
         List<Double> doubleList = new ArrayList<>();
         double toDelete = random.nextDouble();
+
         // when
         for (int i = 0; i < testLength; i++) {
             double randomDouble = random.nextDouble();
             while (doubleList.contains(randomDouble)) {
                 randomDouble = random.nextDouble();
             }
+
             doubleList.add(randomDouble);
             doubleHash.put(randomDouble);
         }
+
         double beforeDelete = doubleHash.getNElem();
         while (doubleList.contains(toDelete)) {
             toDelete = random.nextDouble();
         }
+
         doubleHash.delete(toDelete);
         double afterDelete = doubleHash.getNElem();
 
@@ -377,18 +392,22 @@ public class HashLinearProbingTest {
         List<Double> doubleList = new ArrayList<>();
         int indexToDelete = random.nextInt(testLength);
         double toDelete = 0;
+
         // when
         for (int i = 0; i < testLength; i++) {
             double randomDouble = random.nextDouble();
             while (doubleList.contains(randomDouble)) {
                 randomDouble = random.nextDouble();
             }
+
             if (i == indexToDelete) {
                 toDelete = randomDouble;
             }
+
             doubleList.add(randomDouble);
             doubleHash.put(randomDouble);
         }
+
         int beforeDelete = doubleHash.getNElem();
         doubleHash.delete(toDelete);
         int afterDelete = doubleHash.getNElem();
@@ -409,18 +428,22 @@ public class HashLinearProbingTest {
         List<String> stringList = new ArrayList<>();
         int indexToDelete = random.nextInt(testLength);
         String toDelete = "";
+
         // when
         for (int i = 0; i < testLength; i++) {
             String randomString = RandomStringUtils.randomAlphanumeric(10);
             while (stringList.contains(randomString)) {
                 randomString = RandomStringUtils.randomAlphanumeric(10);
             }
+
             if (i == indexToDelete) {
                 toDelete = randomString;
             }
+
             stringList.add(randomString);
             stringHash.put(randomString);
         }
+
         int beforeDelete = stringHash.getNElem();
         stringHash.delete(toDelete);
         int afterDelete = stringHash.getNElem();
@@ -446,17 +469,19 @@ public class HashLinearProbingTest {
             while (doubleList.contains(randomDouble)) {
                 randomDouble = random.nextDouble();
             }
+
             doubleList.add(randomDouble);
             doubleHash.put(randomDouble);
         }
-        //stringHash.print();
+
         for (int i = 0; i < testLength; i++) {
             doubleHash.delete(doubleList.get(i));
         }
+        double actualNOfElems = doubleHash.getNElem();
+
         //then
-        double expectedSize = 0;
-        double actualSize = doubleHash.getNElem();
-        assertEquals(expectedSize, actualSize, 0);
+        double expectedNOfElems = 0;
+        assertEquals(expectedNOfElems, actualNOfElems, 0);
     }
 
     @Test
@@ -464,6 +489,7 @@ public class HashLinearProbingTest {
         // given
         int testLength = 100;
         List<String> stringList = new ArrayList<>();
+
         // when
         for (int i = 0; i < testLength; i++) {
             String randomString = RandomStringUtils.randomAlphanumeric(10);
@@ -473,14 +499,15 @@ public class HashLinearProbingTest {
             stringList.add(randomString);
             stringHash.put(randomString);
         }
-        //stringHash.print();
+
         for (int i = 0; i < testLength; i++) {
             stringHash.delete(stringList.get(i));
         }
+        double actualNOfElems = stringHash.getNElem();
+
         //then
-        double expectedSize = 0;
-        double actualSize = stringHash.getNElem();
-        assertEquals(expectedSize, actualSize, 0);
+        double expectedNOfElems = 0;
+        assertEquals(expectedNOfElems, actualNOfElems, 0);
     }
 
     @Test
@@ -492,24 +519,28 @@ public class HashLinearProbingTest {
         List<Double> doubleList = new ArrayList<>();
         int indexToDelete = random.nextInt(testLength);
         double toDelete = 0;
+
         //when
         for(int i = 0; i < testLength; i++){
             double randomDouble = random.nextDouble();
             while (doubleList.contains(randomDouble)) {
                 randomDouble = random.nextDouble();
             }
+
             doubleList.add(randomDouble);
             doubleHash.put(randomDouble);
+
             if(i == indexToDelete){
                 toDelete = randomDouble;
             }
         }
-        //then
-        double expectedValueBeforeDelete = toDelete;
         double actualdValueBeforeDelete = doubleHash.get(toDelete);
         doubleHash.delete(toDelete);
-        Object expectedValueAfterDelete = null;
         Object actualdValueAfterDelete = doubleHash.get(toDelete);
+
+        //then
+        double expectedValueBeforeDelete = toDelete;
+        Object expectedValueAfterDelete = null;
         assertEquals(expectedValueBeforeDelete, actualdValueBeforeDelete, 0);
         assertEquals(expectedValueAfterDelete, actualdValueAfterDelete);
     }
@@ -523,26 +554,29 @@ public class HashLinearProbingTest {
         List<String> stringList = new ArrayList<>();
         int indexToDelete = random.nextInt(testLength);
         String toDelete = "";
+
         //when
         for(int i = 0; i < testLength; i++){
             String randomString = RandomStringUtils.randomAlphanumeric(10);
             while (stringList.contains(randomString)) {
                 randomString = RandomStringUtils.randomAlphanumeric(10);
             }
+
             stringList.add(randomString);
             stringHash.put(randomString);
+
             if(i == indexToDelete){
                 toDelete = randomString;
             }
         }
-        //then
-        String expectedValueBeforeDelete = toDelete;
         String actualdValueBeforeDelete = stringHash.get(toDelete);
         stringHash.delete(toDelete);
-        Object expectedValueAfterDelete = null;
         Object actualdValueAfterDelete = stringHash.get(toDelete);
+
+        //then
+        String expectedValueBeforeDelete = toDelete;
+        Object expectedValueAfterDelete = null;
         assertEquals(expectedValueBeforeDelete, actualdValueBeforeDelete);
         assertEquals(expectedValueAfterDelete, actualdValueAfterDelete);
-        
     }
 }
